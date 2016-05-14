6f8fd8775658b5f541dd0fc7bb3b067698db5525
Shahid <smshahidraza@yahoo.com>
2016-05-14 06:39:10 -0400

    Adjust height of slider.

---
+++ js/clockSetting.js
@@ -0,0 +1,168 @@
+function ClockSetting(method) {
+
+	var dailytime =  false;
+	var monthtime = false;
+	return {
+	
+
+		getPrayerClockSetting: function(){
+			var settingObject = prayerTimeAppSetting['SETTING'];					
+			return settingObject ;
+		},
+
+		getName: function(){
+			return this.getPrayerClockSetting()[0].name;
+		},
+
+		getWebsite: function(){
+			return this.getPrayerClockSetting()[3].website;
+		},
+
+		getHijriAdjustmentDay: function(){
+			return this.getPrayerClockSetting()[7].hijriAdjustDay;
+		},
+
+		getMessagseparator: function(arrayLength){
+			if(arrayLength > 1){
+				return this.getPrayerClockSetting()[8].messageseparator;			
+			}else{
+				return "";
+			}
+		},
+
+		getPrayerClockMessage: function(){
+			var messageObject = prayerTimeMessage;
+			return messageObject;
+		},
+
+		getFirstJumaTime: function(){
+			return this.getPrayerClockSetting()[4].juma1;
+		},
+
+		getSecondJumaTime: function(){
+			return this.getPrayerClockSetting()[5].juma2;
+		},
+
+		getAllActiveMessage: function(){
+			var prayerTimeMessage = this.getPrayerClockMessage();
+			var arrayLength = prayerTimeMessage.length;
+			var stringMesage = "";
+			for (var i = 0; i < arrayLength; i++) {
+				var msgarray = prayerTimeMessage[i].message;
+
+				if(msgarray.show === 'yes'){
+					stringMesage = stringMesage + msgarray.text+" "+this.getMessagseparator(arrayLength);
+				}
+			}
+			return stringMesage;
+		},
+
+		getDefaultMessage: function(){
+			var prayerTimeMessage = this.getPrayerClockMessage();
+			var arrayLength = prayerTimeMessage.length;
+			var stringMesage = "";
+			for (var i = 0; i < arrayLength; i++) {
+				if(prayerTimeMessage[i].date == 'DEFAULT'){
+
+				var msgarray = prayerTimeMessage[i].message;
+				for (var j = 0; j < msgarray.length; j++) {
+					var msg = msgarray[j];
+					if(msg.show === 'yes'){
+						stringMesage = stringMesage + msg.text+" "+this.getMessagseparator(arrayLength);
+					}
+				}
+				break;
+				}
+			}
+			return stringMesage;
+		},
+
+		getAllActiveDateMessage: function(){
+			var prayerTimeMessage = this.getPrayerClockMessage();
+			var arrayLength = prayerTimeMessage.length;
+			var stringMesage = "";
+			var currDate = new Date();
+			stringMesage = this.getDefaultMessage();
+			for (var i = 0; i < arrayLength; i++) {
+				var msgarray = prayerTimeMessage[i].message;
+				var day = prayerTimeMessage[i].date;
+				if(day == currDate.getDayName()){
+					var time = prayerTimeMessage[i].time;
+					if(msgarray.show == "yes"){
+						if(time != null){
+							stringMesage = stringMesage + msgarray.text+" "+this.getMessagseparator(arrayLength);	
+						}else{
+							stringMesage = stringMesage + msgarray.text+" "+this.getMessagseparator(arrayLength);	
+						}
+					}
+				}
+				if(day == getDDMMMFromDate(currDate)){
+					var time = prayerTimeMessage[i].time;
+					if(msgarray.show == "yes"){
+						if(time != null){
+							stringMesage = stringMesage + msgarray.text+" "+this.getMessagseparator(arrayLength);	
+						}else{
+							stringMesage = stringMesage + msgarray.text+" "+this.getMessagseparator(arrayLength);	
+						}
+					}
+					
+				}
+			}
+			return stringMesage;
+		},
+
+		/*
+		Slider related method
+		*/
+		getPrayerClockSlider: function(){
+			var slidingObject = prayerTimeAppSetting['SLIDER'];					
+			return slidingObject ;
+		},
+
+		isSlidingOn: function(){
+			return this.getPrayerClockSetting()[6].slider === "on";			
+		},
+
+		getAllActiveSlides: function(){
+			var prayerClockSlider = this.getPrayerClockSlider();
+			var arrayLength = prayerClockSlider.length;
+			var activeSlides = [];
+			for (var i = 0; i < arrayLength; i++) {
+				var show = prayerClockSlider[i].show;
+				var type = prayerClockSlider[i].type;
+				if(show === 'yes'){
+					activeSlides.push(prayerClockSlider[i]);
+
+					/*
+					if(type === 'slides'){
+						activeSlides.push(prayerClockSlider[i]);
+					}else if(type === 'dailytime'){
+						dailytime = true;
+					}else if( type === 'monthtime'){
+						monthtime = true;
+					}
+					*/
+				}
+			}
+			return activeSlides;
+		},
+
+		isDailyTimeOn: function(){
+			return dailytime;
+		},
+
+		isMonthTimeOn: function(){
+			return monthtime;
+		}
+
+	}
+
+
+}
+
+
+
+//---------------------- Init Object -----------------------
+
+
+var clockSetting = new ClockSetting();
